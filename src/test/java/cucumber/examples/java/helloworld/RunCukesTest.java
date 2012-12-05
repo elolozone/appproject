7fde612ffcda6e98f575c9332880e73db171de83
elolozone <github@elolozone.com>
2012-12-05 16:50:15 +0100

    modifs

--- src/test/java/cucumber/examples/java/helloworld/RunCukesTest.java
+++ src/test/java/cucumber/examples/java/helloworld/RunCukesTest.java
@@ -5,7 +5,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 
-@Cucumber.Options(format = { "json", "json:target/cucumber.json" }, features = { "." }) //, strict = true)
+@Cucumber.Options(format = { "json", "json:target/cucumber.json" , "html:target/cucumber/html" }, features = { "." } , strict = true)
+
+
 //@Cucumber.Options(format = { "pretty:target/cucumber/pretty", "html:target/cucumber/html", "json:target/cucumber.json" }, features = { "." })
 
 //@Cucumber.Options(format = {"pretty:target/cucumber/pretty", "html:target/cucumber/html","json:target/cucumber/cucumber.json"}, features = { "." })
