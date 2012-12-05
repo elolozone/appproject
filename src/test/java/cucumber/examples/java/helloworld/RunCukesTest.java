20d70475566168ac83c626dc3727aac48f32dfa9
elolozone <github@elolozone.com>
2012-12-05 15:28:35 +0100

    modifs

--- src/test/java/cucumber/examples/java/helloworld/RunCukesTest.java
+++ src/test/java/cucumber/examples/java/helloworld/RunCukesTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 //@Cucumber.Options(format = { "json", "json:target/cucumber.json" }, features = { "." }, strict = true)
-@Cucumber.Options(format = { "json", "json:target/cucumber.json" }, features = { "." })
+@Cucumber.Options(format = { "pretty:target/cucumber/pretty", "html:target/cucumber/html", "json:target/cucumber.json" }, features = { "." })
 
 //@Cucumber.Options(format = {"pretty:target/cucumber/pretty", "html:target/cucumber/html","json:target/cucumber/cucumber.json"}, features = { "." })
 public class RunCukesTest {
---
+++ target/cucumber/html/report.js
@@ -0,0 +1,826 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("\u0027target/generated-test-sources/cucumber/2.feature\u0027");
+formatter.feature({
+  "id": "connection-to-the-system",
+  "description": "",
+  "name": "Connection to the system",
+  "keyword": "Feature",
+  "line": 1
+});
+formatter.background({
+  "description": "",
+  "name": "",
+  "keyword": "Background",
+  "line": 4,
+  "type": "background"
+});
+formatter.scenario({
+  "id": "connection-to-the-system;correct-login",
+  "tags": [
+    {
+      "name": "@OPEN",
+      "line": 8
+    },
+    {
+      "name": "@DEMO-1",
+      "line": 8
+    }
+  ],
+  "description": "",
+  "name": "correct login",
+  "keyword": "Scenario",
+  "line": 9,
+  "type": "scenario"
+});
+formatter.step({
+  "name": "a user \"Laurent Meurisse\"",
+  "keyword": "Given ",
+  "line": 10
+});
+formatter.step({
+  "name": "his login is \"lmeurisse\" and Password \"coucou\"",
+  "keyword": "And ",
+  "line": 11
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucou\"",
+  "keyword": "When ",
+  "line": 12
+});
+formatter.step({
+  "name": "the system authorize him",
+  "keyword": "Then ",
+  "line": 13
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Laurent Meurisse",
+      "offset": 8
+    }
+  ],
+  "location": "Cool.a_user(String)"
+});
+formatter.result({
+  "duration": 113864000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 14
+    },
+    {
+      "val": "coucou",
+      "offset": 39
+    }
+  ],
+  "location": "Cool.his_login_is_and_Password(String,String)"
+});
+formatter.result({
+  "duration": 234000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucou",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 156000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_authorize_him()"
+});
+formatter.result({
+  "duration": 35000,
+  "status": "passed"
+});
+formatter.background({
+  "description": "",
+  "name": "",
+  "keyword": "Background",
+  "line": 4,
+  "type": "background"
+});
+formatter.scenario({
+  "id": "connection-to-the-system;to-login-error",
+  "tags": [
+    {
+      "name": "@ORPHAN",
+      "line": 16
+    }
+  ],
+  "description": "",
+  "name": "to login error",
+  "keyword": "Scenario",
+  "line": 17,
+  "type": "scenario"
+});
+formatter.step({
+  "name": "a user \"Laurent Meurisse\"",
+  "keyword": "Given ",
+  "line": 18
+});
+formatter.step({
+  "name": "his login is \"lmeurisse\" and Password \"coucou\"",
+  "keyword": "And ",
+  "line": 19
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucoux\"",
+  "keyword": "When ",
+  "line": 20
+});
+formatter.step({
+  "name": "the system display the password fail",
+  "keyword": "Then ",
+  "line": 21
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Laurent Meurisse",
+      "offset": 8
+    }
+  ],
+  "location": "Cool.a_user(String)"
+});
+formatter.result({
+  "duration": 121000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 14
+    },
+    {
+      "val": "coucou",
+      "offset": 39
+    }
+  ],
+  "location": "Cool.his_login_is_and_Password(String,String)"
+});
+formatter.result({
+  "duration": 144000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucoux",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 152000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_display_the_password_fail()"
+});
+formatter.result({
+  "duration": 36000,
+  "status": "passed"
+});
+formatter.background({
+  "description": "",
+  "name": "",
+  "keyword": "Background",
+  "line": 4,
+  "type": "background"
+});
+formatter.scenario({
+  "id": "connection-to-the-system;password-fail-three-times",
+  "tags": [
+    {
+      "name": "@ORPHAN",
+      "line": 24
+    }
+  ],
+  "description": "",
+  "name": "Password fail three times",
+  "keyword": "Scenario",
+  "line": 25,
+  "type": "scenario"
+});
+formatter.step({
+  "name": "a user \"Laurent Meurisse\"",
+  "keyword": "Given ",
+  "line": 26
+});
+formatter.step({
+  "name": "his login is \"lmeurisse\" and Password \"coucou\"",
+  "keyword": "And ",
+  "line": 27
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucoux\"",
+  "keyword": "When ",
+  "line": 28
+});
+formatter.step({
+  "name": "the system display the password fail",
+  "keyword": "Then ",
+  "line": 29
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucoux\"",
+  "keyword": "When ",
+  "line": 30
+});
+formatter.step({
+  "name": "the system display the password fail",
+  "keyword": "Then ",
+  "line": 31
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucoux\"",
+  "keyword": "When ",
+  "line": 32
+});
+formatter.step({
+  "name": "the system display that the account is locked",
+  "keyword": "Then ",
+  "line": 33
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucou\"",
+  "keyword": "When ",
+  "line": 34
+});
+formatter.step({
+  "name": "the system display that the account is locked",
+  "keyword": "Then ",
+  "line": 35
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Laurent Meurisse",
+      "offset": 8
+    }
+  ],
+  "location": "Cool.a_user(String)"
+});
+formatter.result({
+  "duration": 130000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 14
+    },
+    {
+      "val": "coucou",
+      "offset": 39
+    }
+  ],
+  "location": "Cool.his_login_is_and_Password(String,String)"
+});
+formatter.result({
+  "duration": 153000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucoux",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 142000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_display_the_password_fail()"
+});
+formatter.result({
+  "duration": 27000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucoux",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 144000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_display_the_password_fail()"
+});
+formatter.result({
+  "duration": 34000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucoux",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 159000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_display_that_the_account_is_locked()"
+});
+formatter.result({
+  "duration": 42000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucou",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 157000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_display_that_the_account_is_locked()"
+});
+formatter.result({
+  "duration": 794000,
+  "status": "failed",
+  "error_message": "org.junit.ComparisonFailure: expected:\u003c[account locked]\u003e but was:\u003c[OK]\u003e\n\tat org.junit.Assert.assertEquals(Assert.java:125)\n\tat org.junit.Assert.assertEquals(Assert.java:147)\n\tat cucumber.examples.java.helloworld.Cool.the_system_display_that_the_account_is_locked(Cool.java:73)\n\tat ✽.Then the system display that the account is locked(target/generated-test-sources/cucumber/2.feature:35)\n"
+});
+formatter.background({
+  "description": "",
+  "name": "",
+  "keyword": "Background",
+  "line": 4,
+  "type": "background"
+});
+formatter.scenario({
+  "id": "connection-to-the-system;password-fail-2x-but-good-after",
+  "tags": [
+    {
+      "name": "@ORPHAN",
+      "line": 38
+    }
+  ],
+  "description": "",
+  "name": "password fail 2x but good after",
+  "keyword": "Scenario",
+  "line": 39,
+  "type": "scenario"
+});
+formatter.step({
+  "name": "a user \"Laurent Meurisse\"",
+  "keyword": "Given ",
+  "line": 40
+});
+formatter.step({
+  "name": "his login is \"lmeurisse\" and Password \"coucou\"",
+  "keyword": "And ",
+  "line": 41
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucoux\"",
+  "keyword": "When ",
+  "line": 42
+});
+formatter.step({
+  "name": "the system display the password fail",
+  "keyword": "Then ",
+  "line": 43
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucoux\"",
+  "keyword": "When ",
+  "line": 44
+});
+formatter.step({
+  "name": "the system display the password fail",
+  "keyword": "Then ",
+  "line": 45
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucou\"",
+  "keyword": "When ",
+  "line": 46
+});
+formatter.step({
+  "name": "the system authorize him",
+  "keyword": "Then ",
+  "line": 47
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Laurent Meurisse",
+      "offset": 8
+    }
+  ],
+  "location": "Cool.a_user(String)"
+});
+formatter.result({
+  "duration": 128000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 14
+    },
+    {
+      "val": "coucou",
+      "offset": 39
+    }
+  ],
+  "location": "Cool.his_login_is_and_Password(String,String)"
+});
+formatter.result({
+  "duration": 140000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucoux",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 141000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_display_the_password_fail()"
+});
+formatter.result({
+  "duration": 33000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucoux",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 131000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_display_the_password_fail()"
+});
+formatter.result({
+  "duration": 32000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucou",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 145000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_authorize_him()"
+});
+formatter.result({
+  "duration": 33000,
+  "status": "passed"
+});
+formatter.background({
+  "description": "",
+  "name": "",
+  "keyword": "Background",
+  "line": 4,
+  "type": "background"
+});
+formatter.scenario({
+  "id": "connection-to-the-system;password-fail-2x-,-good-after,-and-fail-2x-again-,-good-after",
+  "tags": [
+    {
+      "name": "@ORPHAN",
+      "line": 50
+    }
+  ],
+  "description": "",
+  "name": "password fail 2x , good after, and fail 2x again , good after",
+  "keyword": "Scenario",
+  "line": 51,
+  "type": "scenario"
+});
+formatter.step({
+  "name": "a user \"Laurent Meurisse\"",
+  "keyword": "Given ",
+  "line": 52
+});
+formatter.step({
+  "name": "his login is \"lmeurisse\" and Password \"coucou\"",
+  "keyword": "And ",
+  "line": 53
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucoux\"",
+  "keyword": "When ",
+  "line": 54
+});
+formatter.step({
+  "name": "the system display the password fail",
+  "keyword": "Then ",
+  "line": 55
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucoux\"",
+  "keyword": "When ",
+  "line": 56
+});
+formatter.step({
+  "name": "the system display the password fail",
+  "keyword": "Then ",
+  "line": 57
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucou\"",
+  "keyword": "When ",
+  "line": 58
+});
+formatter.step({
+  "name": "the system authorize him",
+  "keyword": "Then ",
+  "line": 59
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucoux\"",
+  "keyword": "When ",
+  "line": 60
+});
+formatter.step({
+  "name": "the system display the password fail",
+  "keyword": "Then ",
+  "line": 61
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucoux\"",
+  "keyword": "When ",
+  "line": 62
+});
+formatter.step({
+  "name": "the system display the password fail",
+  "keyword": "Then ",
+  "line": 63
+});
+formatter.step({
+  "name": "he wants to be connected with this login \"lmeurisse\" and password \"coucou\"",
+  "keyword": "When ",
+  "line": 64
+});
+formatter.step({
+  "name": "the system authorize him",
+  "keyword": "Then ",
+  "line": 65
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Laurent Meurisse",
+      "offset": 8
+    }
+  ],
+  "location": "Cool.a_user(String)"
+});
+formatter.result({
+  "duration": 206000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 14
+    },
+    {
+      "val": "coucou",
+      "offset": 39
+    }
+  ],
+  "location": "Cool.his_login_is_and_Password(String,String)"
+});
+formatter.result({
+  "duration": 148000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucoux",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 130000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_display_the_password_fail()"
+});
+formatter.result({
+  "duration": 30000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucoux",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 154000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_display_the_password_fail()"
+});
+formatter.result({
+  "duration": 29000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucou",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 156000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_authorize_him()"
+});
+formatter.result({
+  "duration": 31000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucoux",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "duration": 140000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Cool.the_system_display_the_password_fail()"
+});
+formatter.result({
+  "duration": 228000,
+  "status": "failed",
+  "error_message": "org.junit.ComparisonFailure: expected:\u003c[password fail]\u003e but was:\u003c[account locked]\u003e\n\tat org.junit.Assert.assertEquals(Assert.java:125)\n\tat org.junit.Assert.assertEquals(Assert.java:147)\n\tat cucumber.examples.java.helloworld.Cool.the_system_display_the_password_fail(Cool.java:65)\n\tat ✽.Then the system display the password fail(target/generated-test-sources/cucumber/2.feature:61)\n"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucoux",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "Cool.the_system_display_the_password_fail()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "lmeurisse",
+      "offset": 42
+    },
+    {
+      "val": "coucou",
+      "offset": 67
+    }
+  ],
+  "location": "Cool.he_wants_to_be_connected_with_this_login_and_password(String,String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "Cool.the_system_authorize_him()"
+});
+formatter.result({
+  "status": "skipped"
+});
+});
