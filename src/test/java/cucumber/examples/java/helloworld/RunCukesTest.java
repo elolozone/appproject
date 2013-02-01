de11fcaed662fbf19552a8a7a7691cf92cd8fa94
elolozone <github@elolozone.com>
2013-02-01 12:14:41 +0100

    CALC-1

--- src/test/java/cucumber/examples/java/helloworld/RunCukesTest.java
+++ src/test/java/cucumber/examples/java/helloworld/RunCukesTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 //@Cucumber.Options(format = { "json", "json:target/cucumber.json" , "html:target/html" , "pretty:target/pretty" }, features = { "." } )//, strict = true)
 
-@Cucumber.Options(tags = {"~@wip", "~@manual"}, format = {"pretty", "html:src/target/cucumber/html/cucumber-html-report", "json:src/target/cucumber/cucumber.json"})
+@Cucumber.Options(tags = {"~@wip", "~@manual"}, format = {"pretty", "html:src/target/cucumber/html/cucumber-html-report", "json:src/target/cucumber/cucumber.json"}, features = { "." } )
 
 //@Cucumber.Options(format = { "pretty:target/cucumber/pretty", "html:target/cucumber/html", "json:target/cucumber.json" }, features = { "." })
 
